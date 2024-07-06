32190aed3f10e4f208576dde6af03bfa58a519f7
Fahad Mohd Khan <fahad.postbox@gmail.com>
2024-07-06 15:44:15 +0530

    new pages added

--- Home.py
+++ Home.py
@@ -4,7 +4,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/image.png.jpg", width=400)
+    st.image("images/IMG-20240404-WA0009.jpg", width=400)
 
 with col2:
 
