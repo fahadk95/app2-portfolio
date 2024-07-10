598a58bdd201e4fd75fa60c999ff79d7da66987c
Fahad Mohd Khan <fahad.postbox@gmail.com>
2024-07-10 16:48:01 +0530

    send email using forum

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -5,8 +5,13 @@ st.header("Contact me")
 
 with st.form(key="email_forms"):
     user_email = st.text_input("Your Email Address")
-    message = st.text_area("Your Message")
-    message = message + "/n" + user_email
+    raw_message = st.text_area("Your Message")
+    message = f"""/
+    Subject: New email from {user_email}
+    From: {user_email}
+    {raw_message}
+    """
     button = st.form_submit_button()
     if button:
         send_email(message)
+        st.info("Your Email was sent!")
